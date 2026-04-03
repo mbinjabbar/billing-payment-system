f18e27c3de71a1a5b9e4fdb3501958a8b1fb251c
Muhammad Bin Jabbar <muhammad.jabbar@ovada.com>
2026-04-03 05:12:16 -0400

    user route put to patch

--- node_backend/src/routes/user.routes.js
+++ node_backend/src/routes/user.routes.js
@@ -11,6 +11,6 @@ router.use(authorize('Admin'));
 router.get('/', getAllUsers);
 router.post('/', createUser);
 router.get('/:id', getUserById);
-router.put('/:id', updateUser);
+router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
 export default router;
