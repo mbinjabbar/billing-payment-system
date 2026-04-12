49ef4ac901def6d5c50cabd85375ea234e94d726
Muhammad Bin Jabbar <muhammadbinjabbar25@gmail.com>
2026-04-12 14:10:47 +0500

    guards added

---
+++ frontend/src/app/core/guards/role.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const roleGuard = (allowedRoles: string[]): CanActivateFn => {
+  return () => {
+    const authService = inject(AuthService);
+
+    const role = authService.getRole();
+
+    if (role && allowedRoles.includes(role)) {
+      return true;
+    }
+
+    authService.clearSession();
+    return false;
+  };
+};
