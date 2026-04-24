b36ca5857b9a118962cd4f2e79283f50e45bf388
Muhammad Bin Jabbar <muhammad.jabbar@ovada.com>
2026-04-24 04:07:43 -0400

    cors

--- laravel_backend/bootstrap/app.php
+++ laravel_backend/bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'filevalidation' => \App\Http\Middleware\FileValidationMiddleware::class,
             'validate'      => \App\Http\Middleware\ValidateRequest::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
